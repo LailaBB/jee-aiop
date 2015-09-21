60f40c93feebd5f8fc718675168f1504752289fa
user <lyviapavy.lp@gmail.com>
2015-09-21 18:04:33 +0200

    first commit

---
+++ src/fr/polytech/todo/servlet/HelloServlet.java
@@ -0,0 +1,63 @@
+package fr.polytech.todo.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/Hello")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.getRequestDispatcher("Form.jsp").forward(request, response);
+		
+		
+//		out.println("<html>");
+//		out.println("<head>");
+//		out.println("<title>Hello Page</title>");
+//		out.println("</head>");
+//		out.println("<body>");
+//		out.println("<h1>Hello " + request.getParameter("name") + " !</h1>");
+//		out.println("</body>");
+//		out.println("</html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>Hello Page</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<h1>Hello " + request.getParameter("firstname")+ " !</h1>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+}
